95c80ceaf108ea8d12661f142370d8f1f711f388
Matt Thompson <mattluthompson@gmail.com>
2020-06-01 15:57:33 -0400

    wsj nav first commit

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import logo from './images/wsj-logo.png';
+import './App.css';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className = "wholeNav">
+      <div>
+        <img src={logo} alt="WSJ Logo" />
+        <p>English Edition | June 1, 2020 | Print Edition | Video | Latest Headlines</p>
+      </div>
+      <Navbar className = "justify-content-center" bg="light" expand="lg">
+        <Nav>
+          <Nav.Link>Home </Nav.Link>
+          <NavDropdown title="World" id="basic-nav-dropdown">
+            <NavDropdown title="Regions" className = "dropdown-menu-right">
+              <NavDropdown.Item href="#action/3.1">Africa</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Asia</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Canada</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">China</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Europe</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Latin America</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Middle East</NavDropdown.Item>
+            </NavDropdown><NavDropdown.Divider />
+            <NavDropdown title="Sections" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Economy</NavDropdown.Item>
+            </NavDropdown><NavDropdown.Divider />
+            <NavDropdown title="More" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">World Video</NavDropdown.Item>
+            </NavDropdown>
+          </NavDropdown>
+          <NavDropdown title="U.S." id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Politics" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Economy" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Business" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="World" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Markets" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Opinion" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Life & Arts" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Real Estate" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="WSJ. Magazine" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
